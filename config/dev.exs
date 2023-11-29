c66adabc90e63a508b320d7354860cf2c6063c95
Gerry Shaw <gerry_shaw@yahoo.com>
2023-11-29 00:14:22 -0800

    Move SQLite files into db subfolder

--- config/dev.exs
+++ config/dev.exs
@@ -2,7 +2,7 @@ import Config
 
 # Configure your database
 config :sartask, Sartask.Repo,
-  database: Path.expand("../sartask_dev.db", Path.dirname(__ENV__.file)),
+  database: Path.expand("../db/sartask_dev.db", Path.dirname(__ENV__.file)),
   pool_size: 5,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true
--- config/test.exs
+++ config/test.exs
@@ -6,7 +6,7 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :sartask, Sartask.Repo,
-  database: Path.expand("../sartask_test.db", Path.dirname(__ENV__.file)),
+  database: Path.expand("../db/sartask_test.db", Path.dirname(__ENV__.file)),
   pool_size: 5,
   pool: Ecto.Adapters.SQL.Sandbox
 
