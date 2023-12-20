f350fce0996fa36eeeb52c54a1ce10de91e25a1e
Gerry Shaw <gerry_shaw@yahoo.com>
2023-12-20 11:47:52 -0800

    Update hex dependencies

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule App.MixProject do
       {:jason, "~> 1.2"},
       {:pdf, "~> 0.6"},
       {:phoenix_ecto, "~> 4.4"},
-      {:phoenix_html, "~> 3.3"},
+      {:phoenix_html, "~> 4.0"},
       {:phoenix_live_dashboard, "~> 0.8.2"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.2"},
