3b8d6ba04e08c9d7afb681441f784660ea1ed647
Gerry Shaw <gerry_shaw@yahoo.com>
2024-01-09 14:33:33 -0800

    Manage tools and env with Mise

--- config/env.example.exs
+++
@@ -1,7 +0,0 @@
-vars = %{
-  # "MAILGUN_API_KEY" => "secret",
-  "MAILGUN_DOMAIN" => "mg.sarduty.com",
-  "MAPBOX_ACCESS_TOKEN" => "pk___"
-}
-
-Enum.each(vars, fn {k, v} -> System.put_env(k, v) end)
