b64227114a0cb56c1228cf1d8747603ed0057d23
Gerry Shaw <gerry_shaw@yahoo.com>
2024-01-09 13:55:55 -0800

    Fix broken tests

--- test/sartask_web/controllers/error_html_test.exs
+++ test/sartask_web/controllers/error_html_test.exs
@@ -5,10 +5,10 @@ defmodule Web.ErrorHTMLTest do
   import Phoenix.Template
 
   test "renders 404.html" do
-    assert render_to_string(Web.ErrorHTML, "404", "html", []) == "Not Found"
+    assert render_to_string(Web.ErrorHTML, "404", "html", []) =~ "Not found"
   end
 
   test "renders 500.html" do
-    assert render_to_string(Web.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(Web.ErrorHTML, "500", "html", []) =~ "Internal server error"
   end
 end
