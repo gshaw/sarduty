fa430b10af5b35eb4b13a2dce602dfa0db8701df
Gerry Shaw <gerry_shaw@yahoo.com>
2026-02-06 15:41:58 -0800

    Improve refresh job error message

--- lib/app/worker/refresh_team_data_worker.ex
+++ lib/app/worker/refresh_team_data_worker.ex
@@ -15,7 +15,7 @@ defmodule App.Worker.RefreshTeamDataWorker do
       :ok
     rescue
       e ->
-        message = Exception.message(e)
+        message = format_error(e)
         Team.update(team, %{d4h_refresh_result: message})
         {:error, message}
     end
@@ -28,4 +28,17 @@ defmodule App.Worker.RefreshTeamDataWorker do
       url -> Req.get(url)
     end
   end
+
+  defp format_error(%MatchError{term: {:error, %Req.Response{} = response}}) do
+    body =
+      case response.body do
+        body when is_binary(body) -> body
+        body when is_map(body) -> Jason.encode!(body)
+        other -> inspect(other)
+      end
+
+    "D4H API error (#{response.status}): #{String.slice(body, 0, 500)}"
+  end
+
+  defp format_error(e), do: Exception.message(e)
 end
